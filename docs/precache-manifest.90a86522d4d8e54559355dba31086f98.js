b1dd2bacbdb3cf3653e4feb6be4c07e7fa33badf
Ellery <hello@ellerynz.com>
2019-02-08 13:20:04 +1300

    Deploy for Github Pages

---
+++ docs/precache-manifest.90a86522d4d8e54559355dba31086f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd4e130d52275dc8a07b",
+    "url": "/tv-guide/js/about.4c5ee2bc.js"
+  },
+  {
+    "revision": "29494f21aecab70e1ea8",
+    "url": "/tv-guide/css/app.81352e30.css"
+  },
+  {
+    "revision": "29494f21aecab70e1ea8",
+    "url": "/tv-guide/js/app.2edea7c9.js"
+  },
+  {
+    "revision": "d234422835213e448971",
+    "url": "/tv-guide/js/chunk-vendors.647b5d09.js"
+  },
+  {
+    "revision": "c8c62f5a96003c2348eefee648fa5e11",
+    "url": "/tv-guide/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tv-guide/robots.txt"
+  }
+];
